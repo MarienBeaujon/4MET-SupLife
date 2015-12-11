78e417426d7c43423231edcac3ae56aaedff8dad
ANDO <ANDO@192.168.1.2>
2015-12-11 16:12:21 +0400

    Student class and repository

--- src/Users/FormBundle/Controller/DefaultController.php
+++ src/Users/FormBundle/Controller/DefaultController.php
@@ -10,4 +10,15 @@ class DefaultController extends Controller
     {
         return $this->render('UsersFormBundle:Default:login.html.twig');
     }
+    
+    // Fonction qui controle le login de chaque Etudiant
+    public function loginAction()
+    {
+        $repository = $this ->getDoctrine()
+                            ->getManager()
+                            ->getRepository('UsersFormBundle:Default');
+        
+        $data = $repository->findAll();
+        return $data;
+    }
 }
