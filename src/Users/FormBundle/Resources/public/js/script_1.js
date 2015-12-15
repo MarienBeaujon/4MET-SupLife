f4049c2832e95f2a675b732ce119642a111ef2a3
Beaujon <marien.beaujon@gmail.com>
2015-12-15 14:45:55 +0400

    MVC

---
+++ src/Users/FormBundle/Resources/public/js/script_1.js
@@ -0,0 +1,24 @@
+function validateForm() {
+    var x = document.forms["loginform"]["email"].value;
+	var y = document.forms["loginform"]["password"].value;
+	
+	if (x == null || x == "" && y == null || y == ""){
+		alert("Please enter your email and password");
+		}
+		return false;
+	}
+	
+$('#loginform').submit(function() {
+    validateEmail($('input').val());
+    return false;
+});
+
+function validateEmail(email) {
+		var priceRegex = /[0-9]{6}@supinfo.com/;
+		
+        if (email.match(priceRegex)) {
+            alert('Logged IN');
+        } else {
+            alert('Please insert a supinfo email');
+        }
+}
